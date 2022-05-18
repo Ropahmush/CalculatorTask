f80b47a86edab063517f76e34590bbaa5682a6df
Ropahmush <ropamusha07@outlook.com>
2022-05-18 18:47:32 +0200

    task number 4

--- Calc.php
+++ Calc.php
@@ -4,4 +4,15 @@ function execute(string $text):void
     echo ($text);
 }
   execute("param3t3r");
+
+
+function repeatContent(int $count = 1):void
+{
+    for($i = 0; $i <= $count; $i++) {
+     execute("me");
+    }
+ }
+ repeatContent();
+ repeatContent(5);
+
 ?>
