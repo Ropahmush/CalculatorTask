d9d73320f102be90138bdeafb619320f109c1a5b
Ropahmush <ropamusha07@outlook.com>
2022-05-21 22:55:41 +0200

    calculator

---
+++ calculator.php
@@ -0,0 +1,28 @@
+<?php
+function sum($num1,$num2){
+    $sum = $num1 + $num2;
+  return $sum;
+}
+
+function diff($num1,$num2){
+    $diff = $num1 - $num2;
+  return $diff;
+}
+
+function mult($num1,$num2){
+    $mult = $num1 * $num2;
+  return $mult;
+}
+
+function div($num1,$num2){
+    $div = $num1 / $num2;
+  return $div;
+}
+
+print(sum(10,2))."<br/>";
+print(diff(10,2));"<br/>";
+print(mult(10,2));"<br/>";
+print(div(10,2));"<br/>";
+?>
+
+
