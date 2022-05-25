7d81ec08eefccbc5bcbca69e8be5a764009b2a59
Ropahmush <ropamusha07@outlook.com>
2022-05-25 15:47:05 +0200

    task8

--- calculator.php
+++ calculator.php
@@ -1,28 +1,57 @@
 <?php
-function sum($num1,$num2){
-    $sum = $num1 + $num2;
+function sum(float ...$nums): float{
+  $sum = 0; 
+  foreach($nums as $num){
+   $sum = ($num+$sum); 
+  }
   return $sum;
 }
-
-function diff($num1,$num2){
-    $diff = $num1 - $num2;
+function diff(float ...$nums): float{
+    $diff = 0;
+    foreach($nums as $num){
+    $diff = $num - $diff;
+    }
   return $diff;
 }
 
-function mult($num1,$num2){
-    $mult = $num1 * $num2;
-  return $mult;
+function mult(float ...$nums):float{
+   $mult = 0;
+   $mult++;
+   foreach($nums as $num){
+       $mult = $num*$mult;
+   }
+        return $mult;
 }
 
-function div($num1,$num2){
-    $div = $num1 / $num2;
+function div(float ...$nums):float{
+    $div = 0;
+    $div++;
+    foreach($nums as $num){
+        $div = $num/$div;
+    }
   return $div;
 }
 
 print(sum(10,2))."<br/>";
-print(diff(10,2));"<br/>";
-print(mult(10,2));"<br/>";
-print(div(10,2));"<br/>";
+print(diff(10,2))."<br/>";
+print(mult(10,2))."<br/>";
+print(div(10,2))."<br/>";
+
+function calculator(float $num1,string $opp,float $num2,): float{
+  if (!($opp == '+' or $opp == '-' or $opp == '*' or $opp == '/')){
+     return 0;
+   } 
+   switch ($opp){
+     case '+': return sum($num1,$num2);
+     case '-': return diff($num1,$num2); 
+     case '*': return mult($num1,$num2);
+     case '/': return div($num1,$num2);
+   }
+}
+echo calculator(1,"+",2)."<br/>";
+echo calculator(1,"-",2)."<br/>";
+echo calculator(1,"*",2)."<br/>";
+echo calculator(1,"/",2)."<br/>";
 
 function calculator(float $num1,string $opp,float $num2,): float{
   if (!($opp == '+' or $opp == '-' or $opp == '*' or $opp == '/')){
