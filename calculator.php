16b33a4f21561331433ef596f82b84d63fd39601
Ropahmush <ropamusha07@outlook.com>
2022-05-25 17:18:29 +0200

    task9

--- calculator.php
+++ calculator.php
@@ -37,36 +37,19 @@ print(diff(10,2))."<br/>";
 print(mult(10,2))."<br/>";
 print(div(10,2))."<br/>";
 
-function calculator(float $num1,string $opp,float $num2,): float{
-  if (!($opp == '+' or $opp == '-' or $opp == '*' or $opp == '/')){
-     return 0;
-   } 
-   switch ($opp){
-     case '+': return sum($num1,$num2);
-     case '-': return diff($num1,$num2); 
-     case '*': return mult($num1,$num2);
-     case '/': return div($num1,$num2);
-   }
-}
-echo calculator(1,"+",2)."<br/>";
-echo calculator(1,"-",2)."<br/>";
-echo calculator(1,"*",2)."<br/>";
-echo calculator(1,"/",2)."<br/>";
+function calculator2(string $userinput): float{
+   
+  echo($userinput);
+  preg_match_all('!\d+!',$userinput,$matches);
+  
+  print_r($matches);
+  
+preg_match_all('!\D+!', $userinput,$operators);
+  
+print_r($operators);
 
-
-function calculator2(float $num1,string $opp,float $num2,):float{
-  if (!in_array($opp, ['+','-','*','/'])){
-    return 0;
-  }
-  $funct_name=['+' => 'sum',
-                    '-' => 'diff',
-                    '*' => 'mult',
-                    '/' => 'div'];
- return $funct_name[$opp]($num1,$num2);   
+return(0);
 }
 
-echo calculator2(1,'+',2);
-
-?>
-
+echo calculator2('3+2*3');
 
