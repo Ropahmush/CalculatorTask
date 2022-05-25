02242775346edd77e84cca2d071499e562a9a058
Ropahmush <ropamusha07@outlook.com>
2022-05-25 15:55:41 +0200

    task8

--- calculator.php
+++ calculator.php
@@ -53,21 +53,6 @@ echo calculator(1,"-",2)."<br/>";
 echo calculator(1,"*",2)."<br/>";
 echo calculator(1,"/",2)."<br/>";
 
-function calculator(float $num1,string $opp,float $num2,): float{
-  if (!($opp == '+' or $opp == '-' or $opp == '*' or $opp == '/')){
-     return 0;
-   } 
-   switch ($opp){
-     case '+': return sum($num1,$num2);
-     case '-': return diff($num1,$num2); 
-     case '*': return mult($num1,$num2);
-     case '/': return div($num1,$num2);
-   }
-}
-echo calculator(1,"+",2)."<br/>";
-echo calculator(1,"-",2)."<br/>";
-echo calculator(1,"*",2)."<br/>";
-echo calculator(1,"/",2)."<br/>";
 
 function calculator2(float $num1,string $opp,float $num2,):float{
   if (!in_array($opp, ['+','-','*','/'])){
